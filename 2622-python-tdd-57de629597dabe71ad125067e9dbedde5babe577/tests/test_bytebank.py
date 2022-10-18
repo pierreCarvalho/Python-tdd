50d856cf811776cd71c5efa9001596ce8838a878
pierre fenner <pierre.fenner@gestaods.com.br>
2022-10-17 21:40:31 -0300

    novo teste automatizado com pytest

---
+++ 2622-python-tdd-57de629597dabe71ad125067e9dbedde5babe577/tests/test_bytebank.py
@@ -0,0 +1,14 @@
+# Given-When-Then metodologia
+# Dado(Contexto)
+# Quando(Ação)
+# Então(Desfecho)
+from codigo.bytebank import Funcionario
+class TestClass:
+    def test_quando_idade_recebe_13_03_2000_deve_retornar_22(self):
+        entrada = "13/03/2000" # Given-Contexto
+        esperado = 22
+
+        funcionario_teste = Funcionario("Teste", entrada, 1111)
+        resultado = funcionario_teste.idade() # When - Ação
+
+        assert resultado == esperado # Then-desfecho
